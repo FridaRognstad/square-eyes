1b7bc612be190d211b073fe4f21189a99a8aa548
FridaRognstad <skyfriro@gmail.com>
2021-04-16 15:09:18 +0200

    .

---
+++ js/producer.js
@@ -0,0 +1,33 @@
+const producerFilmContainer = document.querySelector(".film-section");
+const url = "https://fridarognstad.one/squareeyes/wp-json/wc/store/products";
+
+async function fetchProducerFilms() {
+  try {
+    const response = await fetch(url);
+    const films = await response.json();
+
+    console.log(films);
+
+    producerFilmContainer.innerHTML = "";
+
+    for (let i = 0; i < films.length; i++) {
+      if (films[i].short_description !== "<p>From: Thomas Lewis</p>") {
+        continue;
+      }
+
+      producerFilmContainer.innerHTML += `<a href="watch/watch2.html?id=${films[i].id}">
+                                        <img src="${films[i].images[1].src}" class="filmImageProducer" alt="${films.name}" />
+                                        <div class="play-icon">
+                                        <i class="far fa-play-circle"></i>
+                                        </div>
+                                        <h2>${films[i].name}</h2>
+                                        </a>
+                                        <h3>${films[i].short_description}</h3>
+                                        <p>${films[i].description}</p>`;
+    }
+  } catch (error) {
+    console.log(error);
+    featuredContainer.innerHTML = message("error", error);
+  }
+}
+fetchProducerFilms();
