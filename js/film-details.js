fd0124eeb248b15f672201e6a7ab47a25fac3dac
FridaRognstad <skyfriro@gmail.com>
2021-04-16 13:29:13 +0200

    called products on wc api

---
+++ js/film-details.js
@@ -0,0 +1,45 @@
+const detailContainer = document.querySelector(".film-details");
+
+const queryString = document.location.search;
+
+const params = new URLSearchParams(queryString);
+
+const id = params.get("id");
+
+console.log(id);
+
+const url =
+  "https://fridarognstad.one/squareeyes/wp-json/wc/store/products/" + id;
+
+console.log(url);
+
+async function fetchFilm() {
+  try {
+    const response = await fetch(url);
+    const details = await response.json();
+
+    console.log(details);
+
+    createHtml(details);
+  } catch (error) {
+    console.log(error);
+    detailContainer.innerHTML = message("error", error);
+  }
+}
+
+fetchFilm();
+
+function createHtml(filmDetails) {
+  detailContainer.innerHTML = `<div class="filmCard" id="filmCardWatch">
+                                    <img src="${filmDetails.images[0].src}" class="filmImage" alt="${filmDetails.name}" />
+                                <div class="filmCardBody">
+                                  <h3 class="cardTitle">${filmDetails.name}</h3>       
+                                  <p class="priceCircle">2$</p>
+                                  <p class="cardProducer">${filmDetails.short_description}</p>
+                                  <div class="filmInfo">
+                                  <p class="cardGenre">${filmDetails.categories[0].name}</p>
+                                </div>
+                                  <p class="cardSummary">${filmDetails.description}</p>
+                                </div>
+                              </div>`;
+}
---
+++ js/index.js
@@ -0,0 +1,35 @@
+const featuredContainer = document.querySelector(".newReleases");
+const url = "https://fridarognstad.one/squareeyes/wp-json/wc/store/products";
+
+async function fetchFeaturedFilms() {
+  try {
+    const response = await fetch(url);
+    const films = await response.json();
+
+    console.log(films);
+
+    featuredContainer.innerHTML = "";
+
+    for (let i = 0; i < films.length; i++) {
+      const tags = films[i].tags;
+
+      for (let a = 0; a < tags.length; a++) {
+        if (tags[a].name !== "featured") {
+          continue;
+        }
+
+        featuredContainer.innerHTML += `<div class="filmCard" id="filmCardBig">
+                                        <a href="watch/watch2.html?id=${films[i].id}">
+                                        <img src="${films[i].images[1].src}" id="filmImageBig" alt="${films.name}" />
+                                        <p class="cardTitle">${films[i].name}</p>
+                                        <p class="cardProducer">${films[i].short_description}</p>
+                                        </a>
+                                        </div>`;
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    featuredContainer.innerHTML = message("error", error);
+  }
+}
+fetchFeaturedFilms();
